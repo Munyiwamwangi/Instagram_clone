e42e1fda1acedb3993c37c9fcc0b57b37045d210
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-01 17:08:19 +0300

    file serial.py must be resitered achild app  of the main app instaapp in the app.py to work

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -1,12 +1,10 @@
-from .models import Profile, Image, Comments
+from .models import Profile,Image,Comments
 from django import forms
-
 
 class getProfile(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['infor']
-
 
 class uploadPhoto(forms.ModelForm):
     class Meta:
@@ -17,4 +15,4 @@ class uploadPhoto(forms.ModelForm):
 class Comment(forms.ModelForm):
     class Meta:
         model = Comments
-        exclude = ['user', 'picture']
+        exclude =['user', 'picture']
