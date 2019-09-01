460acc43676f5cd2ad5611723b78e83720786ee8
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-01 16:19:41 +0300

    add prfile updater views and implememted a search image views, no criteria to search by image, s planning to search by image title

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,19 +1,33 @@
-from django.shortcuts import render, redirect
-from .models import Profile, Comment, Posts
+from django.shortcuts import render,redirect
+from .models import Image,Profile,Comments
 from django.contrib.auth.decorators import login_required
+from .forms import getProfile,uploadPhoto,Comment
 
 # Create your views here.
+def welcome(request):
+    images = Image.objects.all()
+    prof=Profile.objects.filter(infor=request.user.id)[0:1]
+    return render(request,'welcome.html',{"images":images,'prof':prof})
+
+def search_image(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = (request.GET.get("image")).title()
+        searched_images = Image.search_by_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'search.html',{"message":message})    
 
 
-def about(request):
-    return render(request, "about.html")
-
-
-def home(request):
-    return render(request, "about.html")
+def index(request):
+    title = "Index Page"
+    return render (request, 'index.html', {"title":title})
 
 @login_required(login_url='/accounts/login/')
-def edit_profile(request):
+def edit_profile_info(request):
     logged_user =request.user.id
     if request.method == 'POST':
         form = getProfile(request.POST,request.FILES)
@@ -27,39 +41,52 @@ def edit_profile(request):
 
     return render(request,'Profile.html',{'form':form})
 
-
 @login_required(login_url='/accounts/login/')
-def comment(request, image_id):
-
-    post = Posts.objects.get(id=image_id)
-
+def Photo(request):
+    logged_user =request.user.id
     if request.method == 'POST':
-        current_user = request.user
-        form = Comment(request.POST)
-        if form.is_valid:
-            comments = form.save(commit=False)
-            comments.user = current_user
-            comments.picture = image.id
-            comments.save()
-
+        form = uploadPhoto(request.POST,request.FILES)
+        if form.is_valid():
+            Photo = form.save(commit=False)
+            Photo.profile = logged_user
+            Photo.save()
             return redirect('welcome')
     else:
-        form = Comment()
+        form = uploadPhoto()
 
-    comments = Comment.objects.filter(picture=image_id).all
+    return render(request,'upload.html',{'form':form})
 
-    return render(request, "comment.html", {'form': form, "image": image, "comments": comments})
+@login_required(login_url='/accounts/login/')
+def comment(request,image_id):
 
+    image = Image.objects.get(id = image_id)
 
-
-def search_results(request):
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = User.search_by_category(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'search.html', {"message": message, "images": searched_images})
-
+    if request.method=='POST':
+        current_user=request.user
+        form=Comment(request.POST)
+        if form.is_valid:
+            comments=form.save(commit=False)
+            comments.user=current_user
+            comments.picture=image.id
+            comments.save()
+            return redirect('welcome')
     else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html', {"message": message})
+        form=Comment()
+    
+    comments = Comments.objects.filter(picture=image_id).all
+    
+    return render(request,"comment.html",{'form':form, "image":image ,"comments":comments})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    users =request.user.id
+    
+    try:
+        profile = Profile.objects.filter(infor=users).first()
+        all_images = Image.objects.filter(infor=request.user.id).all()
+
+    except ObjectDoesNotExist:
+
+        return redirect('welcome')
+
+    return render (request,"edit.html",{"profile":profile,"all_images":all_images})
